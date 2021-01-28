6e0dd60ee866c7e4d37d3ca7a3cfbd0f4ff50cce
BrennoMilanezi <milanezibrenno@gmail.com>
2021-01-28 16:57:23 -0300

    Agora vai

--- public/src/index.js
+++ public/src/index.js
@@ -16,9 +16,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
 require('./app/controllers/index')(app);
-
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
