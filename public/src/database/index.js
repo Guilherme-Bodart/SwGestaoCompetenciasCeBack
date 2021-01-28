cccb5103f59328de2d0365858e67e560463fd7da
BrennoMilanezi <milanezibrenno@gmail.com>
2021-01-28 15:49:21 -0300

    Banco Server SaoPaulo
    Co-Authored-By: Guilherme-Bodart <28104726+Guilherme-Bodart@users.noreply.github.com>
    Co-Authored-By: gmarchezi <26925551+gmarchezi@users.noreply.github.com>
    Co-Authored-By: pauloricardo50 <26925333+pauloricardo50@users.noreply.github.com>

--- public/src/database/index.js
+++ public/src/database/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/bancoteste', {useNewUrlParser: true, useUnifiedTopology: true});
+const URI_PRODUCAO = "mongodb+srv://leds_skills:leds.skills123@ledsskills.bsizx.mongodb.net/producao?retryWrites=true&w=majority"
+const URI_TESTE = "mongodb+srv://leds_skills:leds.skills123@ledsskills.bsizx.mongodb.net/teste?retryWrites=true&w=majority"
+
+mongoose.connect(URI_TESTE, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
