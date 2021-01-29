37345af42a58f85c61b1dd5e650463f3016df53b
BrennoMilanezi <milanezibrenno@gmail.com>
2021-01-29 15:16:32 -0300

    Update subcategoryController.js

--- public/src/app/controllers/subcategoryController.js
+++ public/src/app/controllers/subcategoryController.js
@@ -67,6 +67,42 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:subcategoriaId', async (req, res) => {
+  try {
+
+      var { nome, categoria } = req.query
+      if (nome === undefined || categoria === undefined){
+        var { nome, categoria } = req.body
+      }
+
+      if(await Categoria.findById(categoria)){
+
+          const subcategoria = await SubCategoria.findByIdAndUpdate(req.params.subcategoriaId)
+
+          if(subcategoria){
+            
+            subcategoria.nome = nome;
+            subcategoria.categoria = categoria;
+            
+            await subcategoria.save()
+            
+            return res.send({ subcategoria })
+          
+          }else{
+            return res.status(400).send({ error: 'Erro ao editar a subcategoria - SubCategoria nao encontrada'})
+          }
+      
+      }else{
+
+          return res.status(400).send({ error: 'Erro ao editar a subcategoria - Categoria nao encontrada'})
+
+      }
+
+  } catch (err) {
+      return res.status(400).send({ error: 'Erro ao editar a subcategoria'})
+  }
+});
+
 router.delete('/:subcategoriaId', async (req, res) => {
     try {
       if(req.params.subcategoriaId){
