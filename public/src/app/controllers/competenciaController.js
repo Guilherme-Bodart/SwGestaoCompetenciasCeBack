d9fd7eefc766b91267dd188663e874d397165fff
BrennoMilanezi <milanezibrenno@gmail.com>
2021-03-08 11:43:14 -0300

    Update competenciaController.js

--- public/src/app/controllers/competenciaController.js
+++ public/src/app/controllers/competenciaController.js
@@ -12,42 +12,47 @@ router.use(authMiddleware);
 router.post('/', async (req, res) => {
     try {
 
-        var { item_usuario_projeto, subcategoria, nota } = req.query
-        if (item_usuario_projeto === undefined || subcategoria === undefined){
-          var { item_usuario_projeto, subcategoria, nota } = req.body
+        var { projeto, membro, subcategoria, nota } = req.query
+        if (projeto === undefined || subcategoria === undefined){
+          var { projeto, membro, subcategoria, nota } = req.body
         }
 
-        if(await SubCategoria.findById(subcategoria) && await ItemProjetoUsuario.findById(item_usuario_projeto)){
-            
-            const existe_competencia = await Competencia.findOne({item_usuario_projeto: item_usuario_projeto, subcategoria: subcategoria})
-            if(!existe_competencia){
+        const item_projeto_usuario = await ItemProjetoUsuario.findOne({usuario: membro, projeto: projeto})
+        if(item_projeto_usuario){
+            if(await SubCategoria.findById(subcategoria) && await ItemProjetoUsuario.findById(item_projeto_usuario._id)){
                 
-                const competencia = await Competencia.create({ usuario: req.usuarioId, item_usuario_projeto, subcategoria, nota })
-                
-                await competencia.save()
+                const existe_competencia = await Competencia.findOne({item_usuario_projeto: item_projeto_usuario._id, subcategoria: subcategoria})
+                if(!existe_competencia){
+                    
+                    const competencia = await Competencia.create({ usuario: req.usuarioId, item_usuario_projeto: item_projeto_usuario._id, subcategoria, nota })
+                    
+                    await competencia.save()
 
-                return res.send({ competencia })
-            
+                    return res.send({ competencia })
+                
+                }else{
+
+                    const competencia = await Competencia.findByIdAndUpdate(existe_competencia._id)
+
+                    competencia.nota = nota;
+
+                    await competencia.save()
+
+                    return res.send({ competencia })
+
+                }
+
             }else{
 
-                const competencia = await Competencia.findByIdAndUpdate(existe_competencia._id)
-
-                competencia.nota = nota;
-
-                await competencia.save()
-
-                return res.send({ competencia })
+                return res.status(400).send({ error: 'Erro em criar a competencia'})
 
             }
-
         }else{
-
-            return res.status(400).send({ error: 'Erro em criar a competencia'+err})
-
+            return res.status(400).send({ error: 'Erro em criar a competencia - membro nao encontrado'})
         }
 
     } catch (err) {
-        return res.status(400).send({ error: 'Erro em criar a competencia'+err})
+        return res.status(400).send({ error: 'Erro em criar a competencia'})
     }
 });
 
